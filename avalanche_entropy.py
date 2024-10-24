5d016e9d538ee0e37e661124674a6a98a63a17ba
Sharwan <shrawant@gmail.com>
2024-10-24 11:00:19 +0400

    Update avalanche_entropy.py

--- avalanche_entropy.py
+++ avalanche_entropy.py
@@ -1,6 +1,7 @@
 from claasp.cipher_modules.avalanche_tests import AvalancheTests
 from claasp.cipher_modules.report import Report
 from claasp.ciphers.block_ciphers.aradi_block_cipher import AradiBlockCipher
+
 cipher = AradiBlockCipher()
 test = AvalancheTests(cipher).avalanche_tests(number_of_samples=1000)
 report = Report(test)
