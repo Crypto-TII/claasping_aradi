f559eefe306c14c5b6da1c54456c59b6eea6c561
Sharwan <shrawant@gmail.com>
2024-10-24 11:00:02 +0400

    Update algebraic_tests.py

--- algebraic_tests.py
+++ algebraic_tests.py
@@ -1,5 +1,6 @@
 from claasp.ciphers.block_ciphers.aradi_block_cipher_sbox import AradiBlockCipherSBox
 from claasp.cipher_modules.algebraic_tests import AlgebraicTests
+
 aradi = AradiBlockCipherSBox(number_of_rounds=2)
 test = AlgebraicTests(aradi)
 result=test.algebraic_tests(timeout_in_seconds=10)
