557ccfb6a36b2122a2bb951710d2fe67f91abb52
Sharwan <shrawant@gmail.com>
2024-10-24 11:01:52 +0400

    Update nist_statistical_tests.py

--- nist_statistical_tests.py
+++ nist_statistical_tests.py
@@ -1,6 +1,7 @@
 from claasp.cipher_modules.statistical_tests.nist_statistical_tests import NISTStatisticalTests
 from claasp.cipher_modules.report import Report
 from claasp.ciphers.block_ciphers.aradi_block_cipher import AradiBlockCipher
+
 cipher = AradiBlockCipher()
 test = NISTStatisticalTests(aradi)
 index = [0,1]
