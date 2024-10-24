49ecb772bed29c768dd5a3d48ce5d0b952617ba5
Sharwan <shrawant@gmail.com>
2024-10-24 11:01:12 +0400

    Update differential_trail_serach.py

--- differential_trail_serach.py
+++ differential_trail_serach.py
@@ -1,5 +1,6 @@
 from claasp.ciphers.block_ciphers.aradi_block_cipher_sbox import AradiBlockCipherSBox
 from claasp.cipher_modules.models.sat.sat_models.sat_xor_differential_model import SatXorDifferentialModel
+
 cipher = AradiBlockCipherSBox(number_of_rounds=4)
 model = SatXorDifferentialModel(cipher)
 trail = model.find_lowest_weight_xor_differential_trail()
