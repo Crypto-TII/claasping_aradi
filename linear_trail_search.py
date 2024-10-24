ab178b73c8d3bcdd749037e518331f2f062ab35d
Sharwan <shrawant@gmail.com>
2024-10-24 11:01:36 +0400

    Update linear_trail_search.py

--- linear_trail_search.py
+++ linear_trail_search.py
@@ -1,5 +1,6 @@
 from claasp.ciphers.block_ciphers.aradi_block_cipher_sbox import AradiBlockCipherSBox
 from claasp.cipher_modules.models.sat.sat_models.sat_xor_linear_model import SatXorLinearModel
+
 cipher = AradiBlockCipherSBox(number_of_rounds=4)
 model = SatXorLinearModel(cipher)
 trail = model.find_lowest_weight_xor_linear_trail()
